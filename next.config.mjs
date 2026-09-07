e21afc692ba4cd96d66161ca9b393d147160d4f7
DevaKurniaSetiawan <devakurnias09@gmail.com>
2026-09-08 03:15:04 +0700

    Deploy dummy dashboard to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true"
+const repositoryBasePath = "/Website-Monitoring-sytem-"
+
 const nextConfig = {
+  output: isGitHubPages ? "export" : undefined,
+  basePath: isGitHubPages ? repositoryBasePath : "",
   typescript: {
     ignoreBuildErrors: true,
   },
